f96ac0cd8c0545cc99492b92b0bb270e51d28040
Allen <th1995121@gmail.com>
2021-12-12 23:55:13 -0800

    add hello world

--- test.py
+++ test.py
@@ -1,30 +1,6 @@
-# # # import os
-# # #
-# # # print(os.listdir('/home/allen/FTP'))
-# #
-# #
-# # class tesss:
-# #
-# #
-# #     def fun1(self):
-# #         print('123')
-# #
-# #     def fun2(self):
-# #         self.fun1()
-# #
-# #
-# #
-# # t= tesss()
-# #
-# # t.fun2()
-#
-#
-# fw = open('/home/allen/FTP/temp/' + 'test1.jpg', 'wb')
-# while True:
-#     data =
-#     if data == b'##':
-#         break
-#     fw.write(data)
 import os
 
 print(os.path.exists('/home/allen/FTP'+'112.log'))
+
+print('hello world')
+
